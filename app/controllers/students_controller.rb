09936eef9d99ce035ac7a44e2468c3a84f3f27da
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-28 01:01:35 -0400

    commit tree a03dd27a7ba2f69a3de88c613ce2bc8f22ce245d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # byebug
+    if params[:name]
+    students = Student.all.select{|student| student.to_s.upcase.include?(params[:name].upcase)}
+    else
+      students = Student.all
+    end
+    # students = Student.find_by(first_name: params[:name])
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
